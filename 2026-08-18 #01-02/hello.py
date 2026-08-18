053672aa173ffb3100106bbb3defc11dacce2054
Python <PyCur@TOP.ru>
2026-08-18 19:54:17 +0200

    first commit

---
+++ 2026-08-18 #01-02/hello.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+# Виртуальное окружение
+# python -m venv venv
+# venv\Scripts\activate (Windows)
+# source venv/bin/activate (Linux/MacOS)
+# deactivate для выхода из виртуального окружения
+# Установка библиотек 
+# Глобально python -m pip
+# pip install fastapi uvicorn
+# uvicorn main:app --reload 
+
+app = FastAPI()
+
+#QUERY-параметры http://localhost:8000/hello?name=Alexey
+@app.get("/hello")
+def hello(name: str=""):
+    if name != "":
+        return {"message": f"Hello, {name}!"}
+    return {"message": "Hello!"}
+
+# PATH-параметры
+@app.get("/hello/{name}")
+def hello_name(name: str):
+    return {"message": f"Hello, {name}!"}
